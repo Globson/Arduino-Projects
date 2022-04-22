03e11638cbed6a48a2d13a6cd1497caee40d6c5f
Globson <samuel.sena@ufv.br>
2022-04-22 18:26:43 -0300

    Controle V1.1

--- BombaCS_GeladeiraAberta_controle/BombaCS_GeladeiraAberta_controle.ino
+++ BombaCS_GeladeiraAberta_controle/BombaCS_GeladeiraAberta_controle.ino
@@ -1,8 +1,13 @@
+/*
+Author: Samuel Sena (Globson)
+Designed for refrigerator door control and temperature monitoring.
+*/
+
 #include <Thermistor.h>
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  // Serial.begin(9600);
 
   pinMode(2, OUTPUT);
   pinMode(3, INPUT);
@@ -21,79 +26,9 @@ void setup()
 
   // pinMode(A1,INPUT);
   pinMode(A2, OUTPUT);
+  pinMode(A3, INPUT);
 }
 Thermistor temp(1);
-void segmentoAleatorio()
-{
-  long randNumber = random(7, 14);
-
-  switch (randNumber)
-  {
-  case 7:
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-    break;
-  case 8:
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    digitalWrite(9, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-    break;
-  case 9:
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(9, HIGH);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-    break;
-  case 10:
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(10, HIGH);
-    digitalWrite(11, LOW);
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-    break;
-  case 11:
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(11, HIGH);
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-    break;
-  case 12:
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, HIGH);
-    digitalWrite(13, LOW);
-    break;
-  case 13:
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, LOW);
-    digitalWrite(13, HIGH);
-    break;
-  }
-}
 
 long segmentoInfinitoAtual = 7;
 void segmentoInfinito()
@@ -397,62 +332,12 @@ void animacao()
   delay(50);
 }
 
-void loop()
+void animacaoBombaCs()
 {
-  // put your main code here, to run repeatedly:
-
-  while (digitalRead(3) == 1)
+  if (analogRead(A3) > 299)
   {
-    for (int i = 0; i < 5; i++)
-    {
-      int temperature = temp.getTemp();
-      Serial.print("Temperatura no Sensor eh: ");
-      Serial.print(temperature);
-      Serial.println("*C");
-      if (temperature < 10 && temperature > -10)
-      {
-        if (temperature >= 0)
-        {
-          colocaUmNumero(temperature);
-          delay(1000);
-          animacao();
-        }
-        else
-        {
-          colocaUmNumero(10);
-          delay(1000);
-          colocaUmNumero(-1);
-          delay(150);
-          colocaUmNumero(temperature * -1);
-          delay(1000);
-          animacao();
-        }
-      }
-      else if (temperature >= 10 && temperature < 100)
-      {
-        int primeirodigito = temperature / 10;
-        int segundodigito = temperature - primeirodigito * 10;
-        colocaUmNumero(primeirodigito);
-        delay(1000);
-        colocaUmNumero(-1);
-        delay(150);
-        colocaUmNumero(segundodigito);
-        delay(1000);
-        animacao();
-      }
-      colocaUmNumero(-1);
-      ledRGB('S');
-    }
+    paraDeEncherOSaco(120000);
   }
-  if (digitalRead(3) == 0)
-  {
-    digitalWrite(2, HIGH);
-    delay(60);
-    digitalWrite(2, LOW);
-    Serial.print("Sensor de luz: ");
-    Serial.println(analogRead(A1));
-  }
-
   colocaUmNumero(7);
   ledRGB('R');
   delay(300);
@@ -468,6 +353,10 @@ void loop()
   delay(300);
   colocaUmNumero(-1);
   delay(150);
+  if (analogRead(A3) > 299)
+  {
+    paraDeEncherOSaco(120000);
+  }
   colocaUmNumero(5);
   ledRGB('R');
   delay(300);
@@ -483,17 +372,105 @@ void loop()
   delay(300);
   colocaUmNumero(-1);
   delay(150);
+  if (analogRead(A3) > 299)
+  {
+    paraDeEncherOSaco(120000);
+  }
   colocaUmNumero(8);
   ledRGB('R');
   delay(300);
   colocaUmNumero(-1);
   delay(150);
   ledRGB('S');
+}
+
+void paraDeEncherOSaco(double pause)
+{
+  for (double i = pause; i > 0; i -= 50)
+  {
+    if (digitalRead(3) == 1)
+    {
+      break;
+    }
+    segmentoInfinito();
+    delay(50);
+  }
+  colocaUmNumero(-1);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  while (digitalRead(3) == 1)
+  {
+    if (analogRead(A3) > 299)
+    {
+      for (int i = 0; i < 5; i++)
+      {
+        if (digitalRead(3) == 0)
+        {
+          break;
+        }
+        int temperature = temp.getTemp();
+        // Serial.print("Temperatura no Sensor eh: ");
+        // Serial.print(temperature);
+        // Serial.println("*C");
+        if (temperature < 10 && temperature > -10)
+        {
+          if (temperature >= 0)
+          {
+            colocaUmNumero(temperature);
+            delay(1000);
+            animacao();
+          }
+          else
+          {
+            colocaUmNumero(10);
+            delay(1000);
+            colocaUmNumero(-1);
+            delay(150);
+            colocaUmNumero(temperature * -1);
+            delay(1000);
+            animacao();
+          }
+        }
+        else if (temperature >= 10 && temperature < 100)
+        {
+          int primeirodigito = temperature / 10;
+          int segundodigito = temperature - primeirodigito * 10;
+          colocaUmNumero(primeirodigito);
+          delay(1000);
+          colocaUmNumero(-1);
+          delay(150);
+          colocaUmNumero(segundodigito);
+          delay(1000);
+          animacao();
+        }
+        colocaUmNumero(-1);
+        ledRGB('S');
+      }
+    }
+  }
+  if (digitalRead(3) == 0)
+  {
+    digitalWrite(2, HIGH);
+    delay(60);
+    digitalWrite(2, LOW);
+  }
+  if (analogRead(A3) > 299)
+  {
+    paraDeEncherOSaco(120000);
+  }
+  animacaoBombaCs();
 
   double tempo = 33500; // ms
   double duracaoMinima;
   while (tempo > 1000 && digitalRead(3) == 0)
   {
+    if (analogRead(A3) > 299)
+    {
+      paraDeEncherOSaco(120000);
+    }
     if (tempo > 10000)
     {
       segmentoInfinito();
@@ -515,17 +492,35 @@ void loop()
       intervalo = duracaoMinima;
       tempo -= (intervalo + 60);
     }
-    Serial.print("Intervalo: ");
-    Serial.println(intervalo);
+    // Serial.print("Intervalo: ");
+    // Serial.println(intervalo);
     delay(intervalo);
   }
 
-  if (digitalRead(3) == 0)
+  while (digitalRead(3) == 0)
   {
+    if (analogRead(A3) > 299)
+    {
+      analogWrite(A2, 0);
+      colocaUmNumero(-1);
+      ledRGB('S');
+      paraDeEncherOSaco(120000);
+      colocaUmNumero(0);
+      analogWrite(A2, 130);
+    }
     colocaUmNumero(0);
-
+    analogWrite(A2, 130);
     for (int i = 0; i < 60; i++)
     { // explosao
+      if (analogRead(A3) > 299)
+      {
+        analogWrite(A2, 0);
+        colocaUmNumero(-1);
+        ledRGB('S');
+        paraDeEncherOSaco(120000);
+        colocaUmNumero(0);
+        analogWrite(A2, 130);
+      }
       if (i % 5 == 0)
       {
         ledRGB('G');
@@ -540,10 +535,8 @@ void loop()
       }
       delay(70);
     }
-
-    delay(600);
   }
-
+  analogWrite(A2, 0);
   colocaUmNumero(-1);
   ledRGB('S');
 }
